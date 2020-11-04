3cc3c191bbd34cb4fdec7f901f1df84170858dc6
Nermin Mitkov <nermin.mitkov1998@gmail.com>
2020-11-04 08:23:47 +0200

    Add project files.

---
+++ Shop.Application/Products/GetProducts.cs
@@ -0,0 +1,23 @@
+﻿using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shop.Application.Products
+{
+    class GetProducts
+    {
+        private ApplicationDbContext _ctx;
+
+        public GetProducts(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public void Do()
+        {
+            _ctx.Products.ToList();
+        }
+    }
+}
---
+++ Shop.UI/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shop.Application.Products;
+using Shop.Database;
+
+namespace Shop.UI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private ApplicationDbContext _ctx;
+
+        public IndexModel(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        [BindProperty]
+        public ProductViewModel Product { get; set; }
+        public void OnGet()
+        {
+
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            await new CreateProduct(_ctx).Do(Product);
+
+            return RedirectToPage("Index");
+        }
+    }
+}
