360611f6e7d5c12208e67dc82f67c25ee70c84a2
Nermin Mitkov <nermin.mitkov1998@gmail.com>
2020-11-11 07:41:41 +0200

    Adding CRUD system only for Admin access only. Learning and implementing Vue.js.

---
+++ Shop.Application/ProductsAdmin/DeleteProduct.cs
@@ -0,0 +1,29 @@
+﻿using Shop.Database;
+using Shop.Domain.Models;
+using System;
+using Microsoft.EntityFrameworkCore.Design;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Shop.Application.ProductsAdmin
+{
+    public class DeleteProduct
+    {
+        private ApplicationDbContext _context;
+        public DeleteProduct(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+
+        public async Task Do(int id)
+        {
+            var Product = _context.Products.FirstOrDefault(x => x.Id == id);
+            _context.Products.Remove(Product);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- Shop.Database/ApplicationDbContext.cs
+++ Shop.Database/ApplicationDbContext.cs
@@ -5,6 +5,8 @@ using Microsoft.EntityFrameworkCore.Design;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Shop.Database
 {
@@ -17,5 +19,15 @@ namespace Shop.Database
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<Stock> Stock { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderProduct> OrderProduct { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<OrderProduct>()
+                 .HasKey(x => new { x.ProductId, x.OrderId });
+        }
     }
 }
